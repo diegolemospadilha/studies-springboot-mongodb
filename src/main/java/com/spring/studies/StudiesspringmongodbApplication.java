6b77222ab772048fb4c6f13531d46195a0bf95a7
lemospadilha <dlemos.padilha@gmail.com>
2021-01-08 20:30:02 -0300

    feat: add estrutura basica do projeto

---
+++ src/main/java/com/spring/studies/StudiesspringmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.spring.studies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiesspringmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudiesspringmongodbApplication.class, args);
+	}
+
+}
