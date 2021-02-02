c750b1f80c709e59b7cb580b6560ec0cab9654c6
lemospadilha <dlemos.padilha@gmail.com>
2021-02-02 08:15:59 -0300

    feat: add consulta de post por titulo com @query

--- src/main/java/com/spring/studies/repository/PostRepository.java
+++ src/main/java/com/spring/studies/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.spring.studies.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.spring.studies.domain.Post;
@@ -10,6 +11,10 @@ import com.spring.studies.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
+	
 	public List<Post> findByTitleContainingIgnoreCase(String title);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	public List<Post> findByTitle(String title);
 
 }
